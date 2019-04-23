03927d852f4c3f940f3681883de79feb18e9994c
ddevadat <deepak.devd@outlook.com>
2019-04-23 14:35:10 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@ node puppet.local {
   include role::master
   file { '/etc/secret_password.txt':
       ensure => file,
-      content => lookup(secret_password'),
+      content => lookup('secret_password'),
   
   }
 }
